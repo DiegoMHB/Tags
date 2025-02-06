b4a14fb62c9e3b1408ef3556886fa55227b80cf5
DiegoMHB <dmhb17@gmail.com>
2025-02-06 17:57:04 +0100

    styling landing

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -7,23 +7,29 @@ import { linkList } from "../types/appTypes";
 export default function RootLayout() {
   const [auth, setAuth] = useState<boolean>(false);
 
-  const links : linkList= [
-    {to: "/profile", text:"Profile",id:1},
-    {to: "/posts", text:"Posts",id:2},
-    {to: "/map", text:"Map",id:3},
-  ]
+  const links: linkList = [
+    { to: "/profile", text: "Profile", id: 1 },
+    { to: "/posts", text: "Posts", id: 2 },
+    { to: "/map", text: "Map", id: 3 },
+  ];
 
   return (
     <>
-      <div className="flex flex-col justify-between h-screen  ">
-        <div className="bg-gradient-to-r from-[#c6eef7] to-[#e0f5e6] m-2 mb-0 h-screen rounded-2xl">
-          {auth ? <Map /> : <Landing />}
-          <button onClick={() => setAuth(!auth)}>cheking</button>
+      <div className="flex flex-col justify-between h-screen ">
+        <div className="flex justify-evenly bg-black h-10 items-center ">
+          <button className="bg-white " onClick={() => setAuth(!auth)}>
+            cheking
+          </button>
         </div>
 
-        <div className="flex justify-evenly bg-black h-15 items-center " >
-            {links.map((el)=> 
-            <NavBarElement key={el.id} link={el.to} text={el.text} />)}
+        <div className="flex align-middle justify-center bg-gradient-to-r from-[#c6eef7] to-[#e0f5e6] m-2 mb-0 h-screen rounded-2xl">
+          {auth ? <Map /> : <Landing />}
+        </div>
+
+        <div className="flex justify-evenly bg-black h-15 items-center ">
+          {links.map((el) => (
+            <NavBarElement key={el.id} link={el.to} text={el.text} />
+          ))}
         </div>
       </div>
     </>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,8 +1,13 @@
 export default function Landing() {
   return (
-    <div className="flex flex-row ">
-      <button 
-      className="w-[80%] h-60px rounded-full uppercase text-2xl text-white bg-black my-auto p-3 ">
+    <div className="flex flex-col justify-center items-center w-screen h-screen space-y-4">
+      <button className="w-[80%]  rounded-full uppercase text-2xl text-white bg-black ">
+        Check Around
+      </button>
+      <button className="w-[80%] rounded-full uppercase text-2xl text-white bg-black ">
+        Check Around
+      </button>
+      <button className="w-[80%] rounded-full uppercase text-2xl text-white bg-black ">
         Check Around
       </button>
     </div>
