710313bff4e932025bb7f0bd96ea8da3be8e09e5
DiegoMHB <dmhb17@gmail.com>
2025-02-07 16:24:28 +0100

    undone

--- src/components/NavBarElement.tsx
+++ src/components/NavBarElement.tsx
@@ -1,23 +1,13 @@
 import { NavLink } from "react-router-dom";
-import { appStore } from "../zustand/appStore";
-
 
 type NavBarElementProps = {
-    text: string;
-    link: string;
+  text: string;
+  link: string;
 };
 
 export default function NavBarElement({ text, link }: NavBarElementProps) {
-
-    const { auth } = appStore();
-    
   return (
-
-    <div
-      className={` px-5 py-2 flex justify-center items-center bg-[#c6eef7] rounded-full 
-        ${!auth ? "disabled-link opacity-80" : ""}
-          `}
-    >
+    <div className=" px-5 py-2 flex justify-center items-center bg-[#c6eef7] rounded-full">
       <NavLink className=" text-s text-black" to={link}>
         {text}
       </NavLink>
