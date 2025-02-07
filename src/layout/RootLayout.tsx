df07406c28efa02c4cf29cf7e49a00c83acba39a
DiegoMHB <dmhb17@gmail.com>
2025-02-07 11:37:02 +0100

    styling: fixing heigths, widths of header main and footer

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
   const [auth, setAuth] = useState<boolean>(false);
 
   const links: linkList = [
+    { to: "/home", text: "Home", id: 1 },
     { to: "/profile", text: "Profile", id: 1 },
     { to: "/posts", text: "Posts", id: 2 },
     { to: "/map", text: "Map", id: 3 },
@@ -16,21 +17,24 @@ export default function RootLayout() {
   return (
     <>
       <div className="flex flex-col justify-between h-screen ">
-        <div className="flex justify-evenly bg-black h-10 items-center ">
+        <section className="flex justify-evenly bg-black h-10p items-center ">
           <button className="bg-white " onClick={() => setAuth(!auth)}>
             cheking
           </button>
-        </div>
+        </section>
 
-        <div className="flex align-middle justify-center bg-gradient-to-r from-[#c6eef7] to-[#e0f5e6] m-2 mb-0 h-screen rounded-2xl">
+        <section 
+        className="flex-1 flex align-middle justify-center 
+        bg-gradient-to-r from-[#c6eef7] to-[#e0f5e6] m-2 mb-0 
+        h-[88%] rounded-2xl">
           {auth ? <Map /> : <Landing />}
-        </div>
+        </section>
 
-        <div className="flex justify-evenly bg-black h-15 items-center ">
+        <section className="flex justify-evenly bg-black h-15 items-center ">
           {links.map((el) => (
             <NavBarElement key={el.id} link={el.to} text={el.text} />
           ))}
-        </div>
+        </section>
       </div>
     </>
   );
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,13 @@
 export default function Landing() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen space-y-4">
-      <button className="w-[80%]  rounded-full uppercase text-2xl text-white bg-black ">
+    <div className="flex flex-col justify-center items-center w-screen space-y-4">
+      <button className="w-[80%] max-w-[330px]  rounded-full uppercase text-2xl text-white bg-black ">
         Check Around
       </button>
-      <button className="w-[80%] rounded-full uppercase text-2xl text-white bg-black ">
+      <button className="w-[80%] max-w-[330px] rounded-full uppercase text-2xl text-white bg-black ">
         Check Around
       </button>
-      <button className="w-[80%] rounded-full uppercase text-2xl text-white bg-black ">
+      <button className="w-[80%] max-w-[330px] rounded-full uppercase text-2xl text-white bg-black ">
         Check Around
       </button>
     </div>
