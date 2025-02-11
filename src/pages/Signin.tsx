b13235d812680bf5cbcfa8c3ace6fb50a46f139e
DiegoMHB <dmhb17@gmail.com>
2025-02-11 14:39:44 +0100

    creating User object in SignIn form

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,6 +1,37 @@
+//
+
+import { useState } from "react";
 import BtnMain from "../components/buttons/BtnMain";
+import { User } from "../types/userTypes";
+
+const initialUser: User = {
+  name: "",
+  userName: "",
+  email: "",
+  password: "",
+  city: "",
+  profilePicture: null,
+};
 
 export default function Signin() {
+  const [user, setUser] = useState<User>(initialUser);
+
+  function handleChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const data = e.target;
+    setUser((prev: User) => {
+      return {
+        ...prev,
+        [data.name]: data.value,
+      };
+    });
+  }
+
+  function handelSubmit() {}
+
   return (
     <main className="flex flex-col justify-center items-center w-screen space-y-4 ">
       <h3 className="text-2xl">CREATE AN ACCOUNT :</h3>
@@ -10,8 +41,9 @@ export default function Signin() {
             placeholder="...username*"
             className="w-[100%]"
             type="text"
-            name="username"
+            name="userName"
             required
+            onChange={handleChange}
           />
           <input
             placeholder="...name* "
@@ -19,6 +51,7 @@ export default function Signin() {
             type="text"
             name="name"
             required
+            onChange={handleChange}
           />
           <input
             placeholder="...email*"
@@ -26,6 +59,7 @@ export default function Signin() {
             type="email"
             name="email"
             required
+            onChange={handleChange}
           />
           <input
             placeholder="...password*"
@@ -33,22 +67,26 @@ export default function Signin() {
             type="password"
             name="password"
             required
+            onChange={handleChange}
           />
 
-          <select className="w-[100%] text-gray-500" name="city" required>
-            <option value="" className=" text-gray-500">
-              -- Select a City --
-            </option>
-            <option value="" className=" text-gray-500">
+          <select
+            className="w-[100%] text-gray-500"
+            name="city"
+            required
+            onChange={handleChange}
+          >
+            <option className=" text-gray-500">-- Select a City --</option>
+            <option value="Berlin" className=" text-gray-500">
               Berlin
             </option>
-            <option value="" className=" text-gray-500">
+            <option value="London" className=" text-gray-500">
               London
             </option>
-            <option value="" className=" text-gray-500">
+            <option value="Madrid" className=" text-gray-500">
               Madrid
             </option>
-            <option value="" className=" text-gray-500">
+            <option value="Paris" className=" text-gray-500">
               Paris
             </option>
           </select>
@@ -64,7 +102,7 @@ export default function Signin() {
         </div>
 
         <div className=" flex flex-col justify-center items-center w-[100%] my-3">
-          <BtnMain text="Submit" mode={1} link="" />
+          <BtnMain text="Submit" mode={1} link="" onClick={handelSubmit} />
         </div>
       </form>
       <span id="fileName" className=" ">
---
+++ src/types/userTypes.ts
@@ -0,0 +1,8 @@
+export type User = {
+    name: string
+    userName :string
+    email : string
+    password : string
+    city : string
+    profilePicture: null | File
+}
