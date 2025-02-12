b398943a5115ed2ac92e2a8af7388a57cf81b309
DiegoMHB <dmhb17@gmail.com>
2025-02-12 17:38:24 +0100

    table in DB

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,10 +1,14 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
+import path from "node:path";
 
 
 dotenv.config();
 
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    dialect: 'postgres', 
+    models: [path.join(__dirname, '/../models')], 
+});
 
 export default db;
