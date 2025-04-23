a676675bd1a090909cc287053a12b259571f359e
DiegoMHB <dmhb17@gmail.com>
2025-04-23 13:44:06 +0200

    deploying

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,17 +18,17 @@ async function connectDB() {
         await db.authenticate();
         await db.sync()
             // TODO: CLOSE EXPIRED POSTS 
-            .then(() => Populate())//-- DEV --
-            .then(() => {
-                if(port === "3000")
-                deleteExpiredPosts();
-                deleteDefaultPosts();
-                console.log("Database:::: deleted expired and default Posts")
-            })
-            .then(() => {
-                createPosts();
-                console.log("Database::: new default Posts created")
-            })
+            // .then(() => Populate())//-- DEV --
+            // .then(() => {
+            //     if(port === "3000")
+            //     deleteExpiredPosts();
+            //     deleteDefaultPosts();
+            //     console.log("Database:::: deleted expired and default Posts")
+            // })
+            // .then(() => {
+            //     createPosts();
+            //     console.log("Database::: new default Posts created")
+            // })
         console.log("Connected with DB")
         app.listen(port, () => {
             console.log(`Example app listening on port  https://localhost:${port}`)
