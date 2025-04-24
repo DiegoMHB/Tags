c1902654c70e7a72645a76ac853170131e4d005f
DiegoMHB <dmhb17@gmail.com>
2025-04-24 09:57:51 +0200

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://transcendent-taffy-8552e6.netlify.app"],
     // origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
     METHODS:["GET","POST","PATCH","DELETE"]
@@ -17,8 +17,8 @@ app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Cache-Control', 'no-store');  // Esto evita el cacheo
-    res.status(204).end(); // Responde sin contenido, pero con los headers
+    res.setHeader('Cache-Control', 'no-store');  //avoids caching
+    res.status(204).end(); // answers with headers
 });
 app.use(cookieParser())
 app.use(bodyParser.json());
