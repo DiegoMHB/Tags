8a1352ec10d43b91e0c5111bc0bec8e83e269b0d
DiegoMHB <dmhb17@gmail.com>
2025-06-06 12:27:16 +0200

    fixing error with CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,22 +9,9 @@ const app = express();
 
 app.use(cors({
     origin: ["http://localhost:5173","https://transcendent-taffy-8552e6.netlify.app"],
-    // origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
-    METHODS:["GET","POST","PATCH","DELETE"]
+    methods:["GET","POST","PATCH","DELETE"]
 }));
-app.options('*', (req, res) => {
-    const origin = req.headers.origin;
-    if (["http://localhost:5173","https://transcendent-taffy-8552e6.netlify.app"].includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Cache-Control', 'no-store');
-    res.status(204).end();
-});
-
 app.use(cookieParser())
 app.use(bodyParser.json());
 app.use(router)
